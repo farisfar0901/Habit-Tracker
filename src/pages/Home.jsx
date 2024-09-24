6ca4dee67683321fcf5cef24b3b3d25485d94631
farisfar0901 <ibrahimishak6520@gmail.com>
2024-09-25 00:19:19 +0530

    Delete src directory

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom';
-
-function Home() {
-  // Redirect to login page
-  return <Navigate to="/login" />;
-}
-export default Home;
